fa8de6ec23a547c812ca3ce7e5e592c85158af7f
Vimlesh <kumarvimlesh991@gmail.com>
2022-01-20 14:29:59 +0530

    first commit

---
+++ library.js
@@ -0,0 +1,11 @@
+export let message = "ES6 Modules";
+export function user(name){
+    console.log(`hello ${name}`);
+}
+
+export class test {
+    constructor(){
+        console.log("constructor method");
+
+    }
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+import { message, user, test } from "./library.js";
+console.log(message);
+
+  
+document.body.innerHTML= message; 
+ 
+user("Vimlesh");
+
+let a = new test();
+
+
